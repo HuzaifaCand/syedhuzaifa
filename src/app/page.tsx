8a35d524994256483f4381f16308f5f18036db31
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-09-13 23:05:10 +0500

    first commit, stuff setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home | Syed Huzaifa",
+  description:
+    "Huzaifa: I love to build things that help - Problem Solver and Entrepreneur",
+};
+
 export default function Home() {
   return <section className="min-h-screen"></section>;
 }
